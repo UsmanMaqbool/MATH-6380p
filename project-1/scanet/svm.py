3a7e665ff21c291f48f0c32771b0f4c63ef92db1
usmanmaqbool <usmanmaqbool@outlook.com>
2018-09-26 00:31:17 +0800

    26Sep18

--- project-1/scanet/svm.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-style.use("ggplot")
-from sklearn import svm
-x = [1, 5, 1.5, 8, 1, 9]
-y = [2, 8, 1.8, 8, 0.6, 11]
-plt.scatter(x,y)
-plt.show()
